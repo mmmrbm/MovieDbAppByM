5493c42a107c3ef3489802563d811c168e38569f
Madhuranga Mudannayake <mmmrbm@gmail.com>
2018-10-13 20:21:45 +0530

    Fixed the bug of selected movie poster not being visible in scrolling list

---
+++ MovieDbAppByM/CustomControl/SelectedFocusingListBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace MovieDbAppByM.CustomControl
+{
+    public class SelectedFocusingListBox : ListBox
+    {
+        public SelectedFocusingListBox() : base()
+        {
+            SelectionChanged += new SelectionChangedEventHandler(ListBoxScroll_SelectionChanged);
+        }
+
+        void ListBoxScroll_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ScrollIntoView(SelectedItem);
+        }
+    }
+}
